69a5bc55d44fcd2ecd743b725aea1b92dffab9a4
boris_from_south <sashaborisenko@tut.by>
2022-12-26 17:15:56 +0300

    Add: first controller and view
    Add first controller and view, connect read/write to database

---
+++ StoneShop/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoneShop.Data;
+using StoneShop.Models;
+using System.Collections.Generic;
+
+namespace StoneShop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dataBase;
+
+        public CategoryController(ApplicationDbContext dataBase)
+        {
+            _dataBase = dataBase;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _dataBase.Category;
+            return View(objList);
+        }
+    }
+}
